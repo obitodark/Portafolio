d670ec772a646111f6d541eebd3154e22652b070
willians ccallo <mayhixccallo@gmail.com>
2023-02-04 16:50:56 -0500

    add link de production

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,4 +1,5 @@
-export const url_base = 'http://localhost:8000';
+// export const url_base = 'http://localhost:8000';
+export const url_base = 'https://porta-folio-back-end-node-mongo.onrender.com';
 
 export const getToken = () => {
     const token = `Bearer ${localStorage.getItem('token')}`;
